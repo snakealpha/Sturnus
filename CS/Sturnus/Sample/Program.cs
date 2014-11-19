9f2b3d1b16a8e92e0ab90adbc5675d3f82f34072
Snake Liu <yiyiyouxia@live.cn>
2014-11-19 22:21:23 +0800

    A sample project.

---
+++ CS/Sturnus/Sample/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Elecelf.Sturnus;
+
+namespace Sample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Expression sample1 = Parser.Parse(" 1 + 1 ");
+        }
+    }
+}
